d711dc1c9d23d9d728eb595942c9f616156aa19c
MalikaSajjad <s.malikasajjad@gmail.com>
2021-08-17 09:22:00 +0500

    redis_Islands

---
+++ gop/redis_Island/deploy_resources.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+minikube ssh sudo mkdir /redis01 /redis02 /redis03 /redis04 /redis05 /redis06
+
+kubectl apply -f redis01-pv.yaml
+kubectl apply -f redis02-pv.yaml
+kubectl apply -f redis03-pv.yaml
+kubectl apply -f redis04-pv.yaml
+kubectl apply -f redis05-pv.yaml
+kubectl apply -f redis06-pv.yaml
+
