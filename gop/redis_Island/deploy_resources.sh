8ad113ecc5b60cc3681a997020189752d412d974
MalikaSajjad <s.malikasajjad@gmail.com>
2021-08-17 10:10:18 +0500

    redis_Islands

--- gop/redis_Island/deploy_resources.sh
+++ gop/redis_Island/deploy_resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube ssh sudo mkdir /redis01 /redis02 /redis03 /redis04 /redis05 /redis06
+minikube ssh sudo mkdir /redis01 /redis02 /redis03 /redis04 /redis05 /redis06 ; exit
 
 kubectl apply -f redis01-pv.yaml
 kubectl apply -f redis02-pv.yaml
