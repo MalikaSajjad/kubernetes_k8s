38ff99edb42f76fbb59c047b8d264b6e1c06da6d
MalikaSajjad <s.malikasajjad@gmail.com>
2021-08-20 08:57:21 +0500

    redisIslands

--- gop/redis_Island/deploy_resources.sh
+++ gop/redis_Island/deploy_resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube ssh sudo mkdir /redis01 /redis02 /redis03 /redis04 /redis05 /redis06 ; exit
+#minikube ssh sudo mkdir /redis01 /redis02 /redis03 /redis04 /redis05 /redis06 ; exit
 
 kubectl apply -f redis01-pv.yaml
 kubectl apply -f redis02-pv.yaml
@@ -8,4 +8,6 @@ kubectl apply -f redis03-pv.yaml
 kubectl apply -f redis04-pv.yaml
 kubectl apply -f redis05-pv.yaml
 kubectl apply -f redis06-pv.yaml
-
+kubectl apply -f redis-cluster-service.yaml
+kubectl apply -f redis-cluster-configmap.yaml
+kubectl apply -f redis-cluster.yaml
