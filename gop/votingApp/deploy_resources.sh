8e74af3f02d82d57e87870b8366fd2122a4977cd
MalikaSajjad <s.malikasajjad@gmail.com>
2021-08-20 12:36:16 +0500

    votingApp

---
+++ gop/votingApp/deploy_resources.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl apply -f vote-service.yaml
+kubectl apply -f vote-deployment.yaml
+kubectl apply -f redis-service.yaml
+kubectl apply -f vote-deployment.yaml
+kubectl apply -f db-service.yaml
+kubectl apply -f db-deployment.yaml
+kubectl apply -f worker.yaml
+kubectl apply -f result-service.yaml
+kubectl apply -f result-deployment.yaml
